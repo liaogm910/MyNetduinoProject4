b2a1a5e2836cfa12c2424cef4cb2910dd68e4a36
liaogm910 <liaogm910@outlook.com>
2019-04-27 14:31:01 +0800

    加入專案檔案。

---
+++ MyNetduinoProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace MyNetduinoProject
+{
+    public class Program
+    {
+
+        static OutputPort _led = new OutputPort(Pins.ONBOARD_LED, false);
+
+        static InterruptPort _button = new InterruptPort((Cpu.Pin)0x15, false, Port.ResistorMode.Disabled, 
+                                                                               Port.InterruptMode.InterruptEdgeBoth);
+
+        public static void Main()
+        {
+            _led.Write(false);
+
+            _button.OnInterrupt += HandleButtonClick;
+
+            while (true)
+            {
+
+            }
+        }
+
+        static void HandleButtonClick(uint port, uint data, DateTime time)
+        {
+            Debug.Print("Data: " + data.ToString());
+            _led.Write(data == 1);
+        }
+
+    }
+}
